75ffabdd86ca0e83c2e80e561acb2c766fbf4818
gautamchandra869 <79848546+gautamchandra869@users.noreply.github.com>
2021-04-02 10:10:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,90 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	var option={
+		isStatic:true
+	}
+
+	
+
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10,);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+	
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	
+	box01Sprite=createSprite(250,605,30,130);
+	box01Sprite.shapeColor=color("red");
+
+
+	box02Sprite=createSprite(550,605,30,130);
+	box02Sprite.shapeColor=color("red");
+
+	box03Sprite=createSprite(400,655,270,30);
+	box03Sprite.shapeColor=color("red");
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+
+	
+
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+	 Matter.Body.setStatic(packageBody,false)
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+
+    
+  }
+}
+
+
+
